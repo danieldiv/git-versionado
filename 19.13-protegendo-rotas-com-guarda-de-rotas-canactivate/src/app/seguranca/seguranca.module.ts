d0e673e54afadb67a5db40b5af70e4180c642857
Daniel <das.29@outlook.com>
2019-11-18 14:19:10 -0300

    codigo-fonte

---
+++ 19.13-protegendo-rotas-com-guarda-de-rotas-canactivate/src/app/seguranca/seguranca.module.ts
@@ -0,0 +1,50 @@
+import { NgModule, Injectable } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+
+import { LoginFormComponent } from './login-form/login-form.component';
+import { SegurancaRoutingModule } from './seguranca-routing.module';
+import { JwtHelperService, JwtModule } from '@auth0/angular-jwt';
+import { MoneyHttpInterceptor } from './money-http-interceptor';
+import { AuthGuard } from './auth.guard';
+
+export function tokenGetter(): string {
+  return localStorage.getItem('token');
+}
+
+@NgModule({
+  declarations: [LoginFormComponent],
+  imports: [
+    JwtModule.forRoot({
+      config: {
+        tokenGetter,
+        whitelistedDomains: ['localhost:8080'],
+        blacklistedRoutes: ['http://localhost:8080/oauth/token']
+      }
+    }),
+
+    CommonModule,
+    FormsModule,
+
+    InputTextModule,
+    ButtonModule,
+
+    SegurancaRoutingModule
+  ],
+  exports: [],
+  providers: [
+    JwtHelperService,
+
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MoneyHttpInterceptor,
+      multi: true
+    },
+    AuthGuard
+  ]
+})
+export class SegurancaModule { }
