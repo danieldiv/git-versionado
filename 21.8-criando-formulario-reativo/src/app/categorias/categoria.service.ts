a5041c655de6f953b76592fe521190d5adeb9965
Daniel <das.29@outlook.com>
2019-11-22 16:31:11 -0300

    codigo-fonte

---
+++ 21.8-criando-formulario-reativo/src/app/categorias/categoria.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriaService {
+
+  categoriasUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.categoriasUrl = `${environment.apiUrl}/categorias`;
+  }
+
+  listarTodos(): Promise<any> {
+    const headers = new HttpHeaders().append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
+
+    return this.http.get(`${this.categoriasUrl}`, { headers })
+      .toPromise()
+      .then(response => response);
+  }
+}
