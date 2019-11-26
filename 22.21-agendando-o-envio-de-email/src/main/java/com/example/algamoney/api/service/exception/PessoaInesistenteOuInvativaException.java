4c6215194a6af92b98094baaeffd60942270e013
Daniel <das.29@outlook.com>
2019-11-26 14:10:16 -0300

    Adicionando capitulo 22.

---
+++ 22.21-agendando-o-envio-de-email/src/main/java/com/example/algamoney/api/service/exception/PessoaInesistenteOuInvativaException.java
@@ -0,0 +1,7 @@
+package com.example.algamoney.api.service.exception;
+
+public class PessoaInesistenteOuInvativaException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+}
