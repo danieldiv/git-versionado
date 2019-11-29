139bc2e7996d7e71e63d2bf7f39225820718f3c8
Daniel <das.29@outlook.com>
2019-11-29 17:17:37 -0300

    Adicionando capitulo 23.

---
+++ 23.13-criando-o-formulario-de-contato/src/app/pessoas/pessoas.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { InputMaskModule } from 'primeng/inputmask';
+import { TableModule } from 'primeng/table';
+import { TooltipModule } from 'primeng/tooltip';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { PanelModule } from 'primeng/panel';
+import { DialogModule } from 'primeng/dialog';
+
+import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
+import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
+import { SharedModule } from '../shared/shared.module';
+import { PessoasRountingModule } from './pessoas-routing.module';
+
+@NgModule({
+  declarations: [
+    PessoaCadastroComponent,
+    PessoasPesquisaComponent,
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    PanelModule,
+
+    InputTextModule,
+    InputMaskModule,
+    ButtonModule,
+    TableModule,
+    TooltipModule,
+    DialogModule,
+
+    SharedModule,
+    PessoasRountingModule
+  ],
+  exports: []
+})
+export class PessoasModule { }
