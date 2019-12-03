c758fccaa37ffb74d1fa392dfdb439684c744640
Daniel <das.29@outlook.com>
2019-12-03 16:14:38 -0300

    Adicionando capitulo 24.

---
+++ 24.01-criando-entidades-cidade-e-estado/src/main/java/com/example/algamoney/api/service/PessoaService.java
@@ -0,0 +1,47 @@
+package com.example.algamoney.api.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.example.algamoney.api.model.Pessoa;
+import com.example.algamoney.api.repository.PessoaRepository;
+
+@Service // Sera um componente do spring
+public class PessoaService {
+
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	public Pessoa salvar(Pessoa pessoa) {
+		pessoa.getContatos().forEach(c -> c.setPessoa(pessoa));
+		return this.pessoaRepository.save(pessoa);
+	}
+
+	public Pessoa atualizar(Pessoa pessoa, Long codigo) {
+		Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
+		
+		pessoaSalva.getContatos().clear();
+		pessoaSalva.getContatos().addAll(pessoa.getContatos());
+		pessoaSalva.getContatos().forEach(c -> c.setPessoa(pessoaSalva));
+		
+		
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo", "contatos");
+		return this.pessoaRepository.save(pessoaSalva);
+	}
+
+	public void atualizarPropridadeAtivo(Long codigo, Boolean ativo) {
+		Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
+		pessoaSalva.setAtivo(ativo);
+		this.pessoaRepository.save(pessoaSalva);
+	}
+
+	private Pessoa buscarPessoaPeloCodigo(Long codigo) {
+		Pessoa pessoaSalva = this.pessoaRepository.findOne(codigo);
+		if (pessoaSalva == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		return pessoaSalva;
+	}
+}
