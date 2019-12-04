c6a69138c6ef8807fc60182f3094d4219576f95b
Daniel <das.29@outlook.com>
2019-12-04 09:38:15 -0300

    Adicionando capitulo 25.

---
+++ 25.06-usando-o-growl/src/app/core/model.ts
@@ -0,0 +1,63 @@
+export class Endereco {
+  cep: string;
+  logradouro: string;
+  numero: string;
+  complemento: string;
+  bairro: string;
+  cidade = new Cidade();
+}
+
+export class Contato {
+  codigo: number;
+  nome: string;
+  email: string;
+  telefone: string;
+
+  constructor(codigo?: number,
+              nome?: string,
+              email?: string,
+              telefone?: string) {
+      this.codigo = codigo;
+      this.nome = nome;
+      this.email = email;
+      this.telefone = telefone;
+  }
+
+}
+
+export class Estado {
+  codigo: number;
+  nome: string;
+}
+
+export class Cidade {
+  codigo: number;
+  nome: string;
+  estado = new Estado();
+}
+
+export class Pessoa {
+  codigo: number;
+  nome: string;
+  endereco = new Endereco();
+  ativo = true;
+  contatos = new Array<Contato>();
+}
+
+export class Categoria {
+  codigo: number;
+}
+
+export class Lancamento {
+  codigo: number;
+  tipo = 'RECEITA';
+  descricao: string;
+  dataVencimento: Date;
+  dataPagamento: Date;
+  valor: number;
+  observacao: string;
+  pessoa = new Pessoa();
+  categoria = new Categoria();
+  anexo: string;
+  urlAnexo: string;
+}
